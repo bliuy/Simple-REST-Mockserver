e66fd36f174077fe9c8b010541fbd8b460062d31
Brandon <liuy01510.work@gmail.com>
2022-09-05 02:58:36 +0000

    Added the derivation of the Deserialize trait.

--- src/registration.rs
+++ src/registration.rs
@@ -1,28 +1,32 @@
 use std::collections::HashMap;
 
 use anyhow::Result;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::errors::RegistrationError;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct MockServerPayloadHttpRequestRequestConfig {
     pub(crate) headers: Option<HashMap<String, String>>,
     pub(crate) body: Option<String>,
 }
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
+pub struct MockServerPayloadHttpRequestResponseConfig {
+    pub(crate) headers: Option<HashMap<String, String>>,
+    pub(crate) body: Option<String>,
+}
+#[derive(Debug, Deserialize, Serialize)]
 pub struct MockServerPayloadHttpRequest {
     pub(crate) method: String,
     pub(crate) unique_key: String,
     pub(crate) request_config: MockServerPayloadHttpRequestRequestConfig,
 }
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct MockServerPayloadHttpResponse {
     pub(crate) status_code: u16,
-    pub(crate) headers: Option<HashMap<String, String>>,
-    pub(crate) body: Option<String>,
+    pub(crate) response_config: MockServerPayloadHttpRequestResponseConfig,
 }
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct MockServerPayload {
     pub(crate) http_request: MockServerPayloadHttpRequest,
     pub(crate) http_response: MockServerPayloadHttpResponse,
@@ -32,7 +36,7 @@ pub struct MockServerPayload {
 pub(crate) fn validate_registration_request(request: &MockServerPayload) -> Result<()> {
     // Validating the HTTP method field
     match request.http_request.method.as_str() {
-        "GET" | "POST" | "PUT" | "PATCH" | "DELETE" => {}
+        "GET" | "POST" | "PUT" | "PATCH" => {}
         _ => {}
     }
 
