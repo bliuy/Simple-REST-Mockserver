3ff0c2dc4d42997b3c7b86884c8ca7ed5a7cd3dc
Brandon <liuy01510.work@gmail.com>
2022-09-11 07:55:16 +0000

    Added ResponseErrors enum.

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,4 @@
+use actix_web::ResponseError;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -9,3 +10,29 @@ pub enum RegistrationError {
     #[error("Invalid HTTP status code used: {0}")]
     InvalidHttpStatusCode(u16),
 }
+
+#[derive(Debug, Error)]
+pub enum ResponseErrors {
+    #[error("Error occured in the Redis Backend service. See error response: {0}")]
+    RedisError(String),
+    #[error("The Redis server returned a Nil response.")]
+    RedisNilValue,
+    #[error(
+        "An error was encountered when attempting to convert a Redis value. See error raised: {0}."
+    )]
+    RedisConversionError(String),
+    #[error("Error raised when converting Redis Value to json. See error: {0}")]
+    SerdeJsonConversionError(String),
+    #[error("Matching failed. {0}")]
+    IncorrectDetails(String),
+    #[error("The following error is raised when attempting to retrieve data from Redis: {0}")]
+    MissingInformation(String),
+    #[error(
+        "Expected the following HTTP method: {0}. Got the following HTTP method instead: {1}."
+    )]
+    IncorrectHttpMethod(String, String),
+    #[error("Placeholder Error. Should not be present in the final production code.")]
+    PlaceholderError,
+}
+
+impl ResponseError for ResponseErrors {}
